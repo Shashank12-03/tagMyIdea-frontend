534bdf9701f019024812135b82968d86b537a809
Shashank12-03 <120593640+Shashank12-03@users.noreply.github.com>
2025-07-07 13:58:24 +0530

    deployment changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'https://tagmyidea.el.r.appspot.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // const API_BASE_URL = 'http://localhost:5000';
 
-
+console.log('API_BASE_URL:', API_BASE_URL);
 export async function googleSignIn() {
     try {
         const response = await axios.get(`${API_BASE_URL}/auth/google`);
