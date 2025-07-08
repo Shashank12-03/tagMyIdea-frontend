d6c075a65ec5c71510311d4eb380499e1ee2753f
Shashank12-03 <120593640+Shashank12-03@users.noreply.github.com>
2025-07-08 22:38:45 +0530

    made structure changes some api integrated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = 'http://localhost:5000';
 
 console.log('API_BASE_URL:', API_BASE_URL);
 export async function googleSignIn() {
